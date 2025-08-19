6b32b9f283bd42da10ad0759e882472291b6b1a4
LamofGod01 <lmarshall2800@gmail.com>
2025-08-19 13:43:35 -0700

    updated high score system to read .txt file of high score

--- main.py
+++ main.py
@@ -7,38 +7,51 @@ from player import Player
 from asteroid import Asteroid
 from asteroidfield import AsteroidField
 from shot import Shot
-from high_score import HIGH_SCORE
 
 def main():
     pygame.init()
     print("Starting Asteroids!")
+    print(".")
+    print(".")
+    print(".")
 #    print(f"Screen width: {SCREEN_WIDTH}")
 #    print(f"Screen height: {SCREEN_HEIGHT}")
     Clock = pygame.time.Clock()
     title_font = pygame.font.SysFont(None, 72)
     score_font = pygame.font.SysFont(None, 32)
+
     dt = 0
     score = 0
+
     pygame.display.set_caption("Asteroids Game")
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
-    text_surface = title_font.render("Welcome to Asteroids!", True, "white")
-    text_rect = text_surface.get_rect()
-    text_rect.center = (SCREEN_WIDTH / 2, SCREEN_HEIGHT / 2)
+
+    welcome_surface = title_font.render("Welcome to Asteroids!", True, "white")
+    welcome_rect = welcome_surface.get_rect()
+    welcome_rect.center = (SCREEN_WIDTH / 2, SCREEN_HEIGHT / 2)
+
     updatable = pygame.sprite.Group()
     drawable = pygame.sprite.Group()
     asteroids = pygame.sprite.Group()
     shots = pygame.sprite.Group()
+
     Player.containers = (updatable, drawable)
     Asteroid.containers = (asteroids, updatable, drawable)
     AsteroidField.containers = (updatable)
     Shot.containers = (updatable, drawable, shots)
+
     asteroidfield = AsteroidField()
     player = Player(SCREEN_WIDTH / 2, SCREEN_HEIGHT / 2)
-    screen.blit(text_surface, text_rect)
+
+# display welcome message
+    screen.blit(welcome_surface, welcome_rect)
     pygame.display.flip()
     pygame.time.wait(2000)
+
+    with open("high_score.txt","r") as f:
+        high_score = int(f.read())
     score_surface = score_font.render(f"Current Score: {score}", True, "white")
-    high_score_surface = score_font.render(f"High score: {HIGH_SCORE}", True, "white")
+    high_score_surface = score_font.render(f"High score: {high_score}", True, "white")
     while True:
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
@@ -46,13 +59,14 @@ def main():
         screen.fill("black")
         updatable.update(dt)
         for asteroid in asteroids:
+# game end condition
             if asteroid.collision(player) == True:
                 print("Game over!")
                 print(f"Your score was: {score}")
-                if score > HIGH_SCORE:
+                if score > high_score:
                     print("New high score!")
-                    with open("high_score.py", "w") as file:
-                        file.write(f"HIGH_SCORE = {score}")
+                    with open("high_score.txt", "w") as f:
+                        f.write(f"{score}")
                 return
             for shot in shots:
                 if asteroid.collision(shot):
