6a6e58e52fe93efd7a684e9691e705f1a01be532
LamofGod01 <lmarshall2800@gmail.com>
2025-08-19 11:57:41 -0700

    added asteroid splitting when hit

--- main.py
+++ main.py
@@ -36,7 +36,7 @@ def main():
                 return
             for shot in shots:
                 if asteroid.collision(shot):
-                    asteroid.kill()
+                    asteroid.split()
                     shot.kill()
         for drawn in drawable:
             drawn.draw(screen)
